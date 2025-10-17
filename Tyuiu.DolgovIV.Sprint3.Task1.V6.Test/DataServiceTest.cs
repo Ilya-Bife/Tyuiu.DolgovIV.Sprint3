2d61fb7bc79d88b115cb4bdfd743602cb726eebe
Tyuiu.DolgovIV@outlook.com <Ilya2017.dolgov@yandex.ru>
2025-10-17 16:40:05 +0500

    Task1

---
+++ Tyuiu.DolgovIV.Sprint3.Task1.V6.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.DolgovIV.Sprint3.Task1.V6.Lib;
+
+namespace Tyuiu.DolgovIV.Sprint3.Task1.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 1;
+            int stopValue = 4;
+            int value = 4;
+
+            Assert.AreEqual(ds.GetSumSeries(value, startValue, stopValue), -165.66);
+
+        }
+    }
+}
---
+++ Tyuiu.DolgovIV.Sprint3.Task1.V6/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.DolgovIV.Sprint3.Task1.V6.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Долгов И. В. | АСОиУб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла while                                              *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("* Выполнил: Долгов Илья Витальевич | АСОиУб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу используя цикл while, которая вычисляет              *");
+        Console.WriteLine("* сумму ряда по формуле, при a=4                                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int a = Convert.ToInt32(Console.ReadLine());
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.GetSumSeries(a, x, y));
+
+        Console.ReadKey();
+    }
+}
