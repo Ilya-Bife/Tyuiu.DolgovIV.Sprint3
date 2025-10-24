da5cf28a6fee19c40ed268eb17801aadadcf6e44
Tyuiu.DolgovIV@outlook.com <Ilya2017.dolgov@yandex.ru>
2025-10-24 15:14:04 +0500

    Task4

---
+++ Tyuiu.DolgovIV.Sprint3.Task4.V16.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.DolgovIV.Sprint3.Task4.V16.Lib;
+
+namespace Tyuiu.DolgovIV.Sprint3.Task4.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            int x = -5;
+            int y = 5;
+
+            Assert.AreEqual(ds.Calculate(x, y), 6.719);
+        }
+    }
+}
---
+++ Tyuiu.DolgovIV.Sprint3.Task4.V16/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.DolgovIV.Sprint3.Task4.V16.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Долгов И. В. | АСОиУб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #16                                                             *");
+        Console.WriteLine("* Выполнил: Долгов Илья Витальевич | АСОиУб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+        Console.WriteLine("* функции y=((cos(x)+x)/x)+0.25                                           *");
+        Console.WriteLine("* При х = 0 пропустить значение Полученные значения перемножать.          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(x, y));
+
+        Console.ReadKey();
+    }
+}
